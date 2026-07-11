9ff66dead2d66fa663094fe148c76f6c696bdb88
Shubham Prakash <shubhamprakash681@gmail.com>
2026-07-11 17:55:21 +0530

    Microservice boilerplate with Config server, Euerka server and auth-service

---
+++ common-lib/src/test/java/in/shubhamprakash681/common_lib/CommonLibApplicationTests.java
@@ -0,0 +1,13 @@
+package in.shubhamprakash681.common_lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
