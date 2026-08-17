0e0784ed58aa38fe37771efa4463dd6fce29943c
Shubham Prakash <shubhamprakash681@gmail.com>
2026-08-17 09:22:21 +0530

    Add server configuration to OpenAPI for all services

--- notification-service/src/main/java/in/shubhamprakash681/notification_service/config/OpenApiConfig.java
+++ notification-service/src/main/java/in/shubhamprakash681/notification_service/config/OpenApiConfig.java
@@ -2,6 +2,7 @@ package in.shubhamprakash681.notification_service.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,7 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("TradeX Notification Service")
                         .version("v1")
-                        .description("Watchlist, price alerts, and user notifications APIs"));
+                        .description("Watchlist, price alerts, and user notifications APIs"))
+                .addServersItem(new Server().url("/"));
     }
 }
