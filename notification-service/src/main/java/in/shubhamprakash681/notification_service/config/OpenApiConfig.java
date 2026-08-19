a6adf8f4941f49b042f8cfe1c9a7aea9b7a6e514
Shubham Prakash <shubhamprakash681@gmail.com>
2026-08-19 23:20:06 +0530

    chore: add IntelliJ run configurations and implement JWT security scheme in OpenApiConfig of Notification Service

--- notification-service/src/main/java/in/shubhamprakash681/notification_service/config/OpenApiConfig.java
+++ notification-service/src/main/java/in/shubhamprakash681/notification_service/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package in.shubhamprakash681.notification_service.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,10 +14,14 @@ public class OpenApiConfig {
     @Bean
     OpenAPI notificationOpenApi() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("TradeX Notification Service")
-                        .version("v1")
+                .info(new Info().title("TradeX Notification Service").version("v1")
                         .description("Watchlist, price alerts, and user notifications APIs"))
-                .addServersItem(new Server().url("/"));
+                .addServersItem(new Server().url("/"))
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 }
